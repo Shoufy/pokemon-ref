35be7fc346a6b40a4ac4254786b50192b34d47ba
shoufy <sophie@duck.com>
2023-03-01 17:24:10 +0100

    creation SimulationEntrainement and Combat class

---
+++ entrainement/Combat.java
@@ -0,0 +1,24 @@
+package entrainement;
+
+public class Combat {
+
+    public void shufflePoke() {
+
+    }
+
+    public void initialisationPointsDeVie(int PointsDevie) {
+
+    }
+
+    public void startCombat(String nomArene, int pokemon1, int pokemon2) {
+
+    }
+
+    public void attaque() {
+
+    }
+
+    public void update() {
+
+    }
+}
---
+++ entrainement/SimulationEntrainement.java
@@ -0,0 +1,20 @@
+package entrainement;
+
+public class SimulationEntrainement {
+    public static void main(String[] args) {
+
+    }
+
+    public void afficheAllPokeParOdre() {
+
+    }
+
+    public void afficheDetailPoke() {
+
+    }
+
+    public void afficheAllArene() {
+
+    }
+
+}
